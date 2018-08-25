9083be847d3026493b8da6ee686bc3b9a7c28363
Juan Barbosa <js.barbosa10@uniandes.edu.co>
2018-08-25 22:34:15 +0000

    no interrupted

--- CalibrationTool/micro/main.c
+++ CalibrationTool/micro/main.c
@@ -11,80 +11,42 @@
 
 #define NAME 0x0f
 
-#define EVERY_ADC 86
-
-volatile uint8_t SKIP_ADC = 0;
-volatile uint8_t RESET_INT0 = 0;
 
 void setupADC(void);
 void setupUART(void);
 void setChannel(uint8_t ch);
 void sendADC(uint16_t value);
 
-ISR(ADC_vect)
-{
-	if(SKIP_ADC % EVERY_ADC == 0)
-	{
-		uint16_t value = ADC;
-		sendADC(value);
-	}
-	SKIP_ADC += 1;
-	ADCSRA |= (1 << ADSC);
-}
-
-ISR(INT0_vect)
-{
-	uint8_t val = uart_getc() & (0x7f);
-	
-	GIMSK &= ~(1 << INT0); // disable interrupt INT0
-		
-	if(val == START)
-	{
-		setupADC();
-		ADCSRA |= (1 << ADEN) | (1 << ADSC); // adc enable and start conversion
-	}
-	else if(val == STOP)
-	{
-		ADCSRA &= ~(1 << ADEN);
-	}
-	else if(val == NAME)
-	{
-		uart_puts("Rutherford");
-	}
-	else if((val >= SET_CHANNEL_0) && (val <= SET_CHANNEL_3))
-	{
-		setChannel(val - SET_CHANNEL_0);
-	}
-	else
-	{
-		uart_putc(val);
-	}
-	RESET_INT0 = 1;
-}
-
 int main(void)
 {	
-	setChannel(1);
-	
+	setupADC();
 	setupUART();
 	
-	//~ MCUCR |= (1 << ISC01) | (1 << ISC00); // rising edge on INT0 // NO
-	//~ MCUCR |= (1 << ISC01); // falling edge on INT0 // working first bytes
-	MCUCR |= (1 << ISC00); // falling edge on INT0 
-	GIMSK |= (1 << INT0); // enable interrupt INT0
+	uint8_t val;
 	
 	sei();
-	
-	DDRB |= (1 << PB4);
-	
+		
 	while(1)
 	{		
-		if(RESET_INT0)
+		val = uart_getc();// & (0x7f);
+			
+		if(val == START)
 		{
-			RESET_INT0 = 0;
-			_delay_ms(100);
-			GIFR |= (1 << INTF0);
-			GIMSK |= (1 << INT0); // enable interrupt INT0
+			ADCSRA |= (1 << ADSC); // start conversion
+			while(ADCSRA & (1 << ADSC));
+			sendADC(ADC);
+		}
+		else if(val == NAME)
+		{
+			uart_puts("Rutherford");
+		}
+		else if((val >= SET_CHANNEL_0) && (val <= SET_CHANNEL_3))
+		{
+			setChannel(val - SET_CHANNEL_0);
+		}
+		else
+		{
+			uart_putc(val);
 		}
 	}
 	return 0;
@@ -111,7 +73,7 @@ void setupADC(void)
 	/*setup ADC*/
 	ADMUX |= (1 << REFS0); // internal reference
 	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // set division factor to 128
-	ADCSRA |= (1 << ADATE)| (1 << ADIE); // auto trigger / enable interrupt
+	ADCSRA |= (1 << ADEN);
 }
 
 void setupUART(void)
