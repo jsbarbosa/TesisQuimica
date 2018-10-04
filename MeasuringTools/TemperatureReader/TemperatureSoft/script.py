6cddac9f156d95f9c075679c92a89f559a7371f0
Juan Barbosa <js.barbosa10@uniandes.edu.co>
2018-10-03 20:01:43 -0500

    temperature calibration

--- MeasuringTools/TemperatureReader/TemperatureSoft/script.py
+++ MeasuringTools/TemperatureReader/TemperatureSoft/script.py
@@ -14,9 +14,14 @@ if os.path.exists(FILE_NAME):
     i = 0
     FILE_NAME = "%s_%d%s"%(FILE_ROOT, i, EXTENSION)
     while os.path.exists(FILE_NAME):
+        FILE_NAME = "%s_%d%s"%(FILE_ROOT, i, EXTENSION)
         i += 1
 
-with open(FILE_NAME, "w") as file: pass
+with open(FILE_NAME, "w") as file:
+    file.write("Date\tChannel1\tChannel2\tChannel3\r\n")
+    pass
+
+print("File name:", FILE_NAME)
 
 while True:
     devs = findDevices()
@@ -24,22 +29,26 @@ while True:
         try:
             setGlobalSerial(initPort(devs[0]))
             break
-        except:
+        except Exception as e:
+            print(e)
             setGlobalSerial(None)
             sleep(1)
     else:
+        print(len(devs), devs)
         setGlobalSerial(None)
         sleep(1)
+
+print(devs[0])
 
 while True:
     adjust = TIME / 3 - ADJUST_TIME
     now = strftime("%Y-%m-%d %H:%M:%S", gmtime())
     if adjust < 0: adjust = 0
     try:
-        values = []
+        values = [now]
         for i in range(1, 4):
             setChannel(i)
-            values.append("%.4f" % getVoltage())
+            values.append("%.5f" % getVoltage())
             sleep(adjust)
         with open(FILE_NAME, "a") as file:
             line = "\t".join(values) + "\r\n"
